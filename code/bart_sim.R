2ba3f6bb82c66bf2a7a09b5e7d9f770d80f651e1
OmerRonen <omerronen10@gmail.com>
2022-10-11 12:59:48 -0700

    figs

--- code/bart_sim.R
+++ code/bart_sim.R
@@ -628,16 +628,29 @@ main <- function(args){
   if (plot_type == "cum_sum"){
     cumsum_rmse_plot(ds_name = "breast_tumor", n = 200,p = 1, n_tree = 200
       , nskip = nskip, ndpost = ndpost, nchain=nchain,
-                     add_legend = T,y_lab = "CumSum", synthetic = FALSE, run=2, restricted=restricted)
+                     add_legend = T,y_lab = "CumSum", synthetic = FALSE, run=2, restricted=F)
     cumsum_rmse_plot(ds_name = "breast_tumor", n = Inf,p = 1, n_tree = 200
       , nskip = nskip, ndpost = ndpost, nchain=nchain,
-                     add_legend = F,y_lab = "", synthetic = FALSE, run=2, restricted=restricted)
+                     add_legend = F,y_lab = "", synthetic = FALSE, run=2, restricted=F)
     cumsum_rmse_plot(ds_name = "california_housing", n = 200,p = 1, n_tree = 200
       , nskip = nskip, ndpost = ndpost, nchain=nchain,
-                     add_legend = T,y_lab = "CumSum", synthetic = FALSE, run=3, restricted=restricted)
+                     add_legend = T,y_lab = "CumSum", synthetic = FALSE, run=3, restricted=F)
     cumsum_rmse_plot(ds_name = "california_housing", n = Inf,p = 1, n_tree = 200
       , nskip = nskip, ndpost = ndpost, nchain=nchain,
-                     add_legend = F,y_lab = "", synthetic = FALSE, run=3, restricted=restricted)
+                     add_legend = F,y_lab = "", synthetic = FALSE, run=3, restricted=F)
+
+    cumsum_rmse_plot(ds_name = "breast_tumor", n = 200,p = 1, n_tree = 1
+      , nskip = nskip, ndpost = ndpost, nchain=nchain,
+                     add_legend = T,y_lab = "CumSum", synthetic = FALSE, run=2, restricted=T)
+    cumsum_rmse_plot(ds_name = "breast_tumor", n = Inf,p = 1, n_tree = 1
+      , nskip = nskip, ndpost = ndpost, nchain=nchain,
+                     add_legend = F,y_lab = "", synthetic = FALSE, run=2, restricted=T)
+    cumsum_rmse_plot(ds_name = "california_housing", n = 200,p = 1, n_tree = 1
+      , nskip = nskip, ndpost = ndpost, nchain=nchain,
+                     add_legend = T,y_lab = "CumSum", synthetic = FALSE, run=3, restricted=T)
+    cumsum_rmse_plot(ds_name = "california_housing", n = Inf,p = 1, n_tree = 1
+      , nskip = nskip, ndpost = ndpost, nchain=nchain,
+                     add_legend = F,y_lab = "", synthetic = FALSE, run=3, restricted=T)
   }}
 
 
