ac9b8ad20768429a267b5f9ff465b7d2f378e063
OmerRonen <omerronen10@gmail.com>
2022-10-11 10:39:33 -0700

    new figs

--- gr_plot.sh
+++ gr_plot.sh
@@ -4,5 +4,5 @@
 #SBATCH --mail-user=omer_ronen@berkeley.edu
 #SBATCH -o gr.out #File to which standard out will be written
 #SBATCH -e gr.err #File to which standard err will be written
-
+#SBATCH -p high
 Rscript /accounts/campus/omer_ronen/projects/bart/bart-sims/code/bart_sim.R --n_tree 200 --plot_type gr
