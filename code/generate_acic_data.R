cdc250de794808a1bb0a86863fab0dbaf4cb3c8e
Theo S <theo_s@berkeley.edu>
2023-01-26 19:51:58 -0800

    * Don't write row names

--- code/generate_acic_data.R
+++ code/generate_acic_data.R
@@ -5,24 +5,24 @@ data_2019 <- read.csv("~/Desktop/bart-sims/data/testdataset5.csv")
 
 out_2019 <- read_csv("~/Desktop/bart-sims/data/testdataset5_cf.csv")
 data_2019$Ytrue <- ifelse(data_2019$A, out_2019$EY1_i, out_2019$EY0_i)
-write.csv(data_2019, "~/Desktop/bart-sims/data/ACIC2019.csv")
+write.csv(data_2019, "~/Desktop/bart-sims/data/ACIC2019.csv", row.names = FALSE)
 
 
 # Generate 2018 data -----------------------------------------------------------
 data_2018 <- read.csv("~/Desktop/bart-sims/data/ACIC2018synthetic_data.csv")
 
 # Rename columns
-colnames(raw_data) <- c("schoolid", "Tr","Y","S3","C1","C2","C3","XC","X1","X2","X3","X4","X5")
+colnames(data_2018) <- c("schoolid", "Tr","Y","S3","C1","C2","C3","XC","X1","X2","X3","X4","X5")
 # Generate tau
 tau_true <- function(X) {
   return(0.228 + ifelse(X$X1 < 0.07, .05, 0) + ifelse(X$X2 < -0.69, .05, 0) +
            ifelse(X$C1 %in% c(1,13,14), -.08,0))
 }
 data_tau <- tau_true(data_2018)
-data_2018$Ytrue <- ifelse(data_2018$Z, data_2018$Y+data_tau, data_2018$Y)
+data_2018$Ytrue <- ifelse(data_2018$Tr, data_2018$Y+data_tau, data_2018$Y)
 # Add noise to true potential outcomes to give an SNR of 2
 data_2018$Y <- data_2018$Ytrue + rnorm(nrow(data_2018), sd = sqrt(var(data_2018$Y)/2))
-write.csv(data_2018, "~/Desktop/bart-sims/data/ACIC2018.csv")
+write.csv(data_2018, "~/Desktop/bart-sims/data/ACIC2018.csv", row.names = FALSE)
 
 # Generate 2017 data -----------------------------------------------------------
 
@@ -34,7 +34,7 @@ y_2017 <- read.csv("~/Desktop/bart-sims/data/1.csv", header = FALSE)
 # mu is outcome under no treatment, tau is the cate
 data_2017$Y <- y_2017$V2
 data_2017$Ytrue <- ifelse(y_2017$V1, ytrue_2017$mu+ytrue_2017$alpha, ytrue_2017$mu)
-write.csv(data_2017, "~/Desktop/bart-sims/data/ACIC2017.csv")
+write.csv(data_2017, "~/Desktop/bart-sims/data/ACIC2017.csv", row.names = FALSE)
 
 
 # Generate 2016 data -----------------------------------------------------------
@@ -44,4 +44,4 @@ out_2016 <- read.csv("~/Desktop/bart-sims/data/ACIC2016_zymu_1.csv")
 
 data_2016$Y <- ifelse(out_2016$z, out_2016$y1, out_2016$y0)
 data_2016$Ytrue <- ifelse(out_2016$z, out_2016$mu1, out_2016$mu0)
-write.csv(data_2016,"~/Desktop/bart-sims/data/ACIC2016.csv")
+write.csv(data_2016,"~/Desktop/bart-sims/data/ACIC2016.csv", row.names = FALSE)
