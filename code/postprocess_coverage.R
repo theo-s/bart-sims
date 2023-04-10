83670433d3ae9cf3a0bbaa34df6bba6edd44a730
Theo S <theo_s@berkeley.edu>
2023-04-10 16:20:40 -0700

    * Update plotting code + results for coverage and RMSE sims

--- code/postprocess_coverage.R
+++ code/postprocess_coverage.R
@@ -1,9 +1,7 @@
 library(ggplot2)
 library(dplyr)
 
-# Should be one of sum,high, low, and piecewise
-dgp = "low"
-
+# Unused for now
 calc_winkler <- function(Y,
                          ci.upper,
                          ci.lower) {
@@ -18,72 +16,96 @@ calc_winkler <- function(Y,
   under <- ifelse(Y < ci.lower,
                   ci.lower - Y,0)
   
-  # Winkler 
+  # Winkler
   winkler <- ifelse(over > 0,
                     ci.upper-ci.lower + 40*over, # If over upper limit, pay 2 / .05 * over penalty
                     ifelse(under > 0,
                            ci.upper-ci.lower + 40*over, # If below lower limit, pay 2 / .05 * under penalty
                            ci.upper-ci.lower # Otherwise just pay interval length
-                           )
                     )
+  )
   return(mean(winkler))
   
 }
 
-results <- data.frame(n = NA,
-                      nchain = NA,
-                      run = NA,
-                      exp = 1,
-                      rmse = NA,
-                      cov = NA,
-                      wink = NA)
+all_plots = list()
+
+iteration = 1
+for (dgp in c("sum","high", "low", "piecewise", "tree", "lss")) {
 
 
-# file = "dgp_high_run_8_n_10000_coverage.RDS"
-# f <- readRDS(file = paste0("results/coverage/",file))
+  results <- data.frame(n = NA,
+                        nchain = NA,
+                        run = NA,
+                        exp = 1,
+                        rmse = NA,
+                        cov = NA)
 
-for (file in dir("results/coverage/")) {
-  if (grepl(pattern = dgp, x = file)) {
-    print(file)
-    f <- readRDS(file = paste0("results/coverage/",file))
-    
-    for (iter in 1:3) {
-      results <- rbind(results, 
-                       c(f[[iter]]$n, f[[iter]]$nchain, f[[iter]]$run, 1,
-                         f[[iter]]$rmse, f[[iter]]$empirical_cov, calc_winkler(Y = f[[iter]]$true_y,
-                                                                               ci.upper = f[[iter]]$ci_upper,
-                                                                               ci.lower = f[[iter]]$ci_lower)))
+
+  # file = "dgp_high_run_8_n_10000_coverage.RDS"
+  # f <- readRDS(file = paste0("results/coverage/",file))
+
+  for (file in dir("results/coverage/")) {
+    if (grepl(pattern = dgp, x = file)) {
+      #print(file)
+      f <- readRDS(file = paste0("results/coverage/",file))
+
+      for (iter in 1:3) {
+        n_i = f[[1]]$n
+        run_i = f[[1]]$run
+        nchain_i =f[[1]]$nchain
+        if (is.na(n_i) | is.na(run_i) | is.na(nchain_i)) {
+          print(file)
+          print(f)
+        }
+        if (iter == 1) {
+          results <- rbind(results,
+                           c(n_i, nchain_i, run_i, 1,
+                             f[[iter]]$rmse, f[[iter]]$empirical_cov))
+        } else if (iter == 2) {
+          results <- rbind(results,
+                           c(n_i, 2, run_i, 1,
+                             f[[iter]]$rmse, f[[iter]]$coverage))
+        } else if (iter == 3) {
+          results <- rbind(results,
+                           c(n_i, 5, run_i, 1,
+                             f[[iter]]$rmse, f[[iter]]$coverage))
+        }
+
+      }
     }
   }
+
+  results[-1,] %>%
+    group_by(n, nchain, exp) %>%
+    summarise(reps_completed = max(run)) -> num_runs
+  print(dgp)
+  print(num_runs)
+
+  results[-1,] %>%
+    filter(n>100) %>%
+    group_by(n, nchain, exp) %>%
+    summarise(mean_rmse = mean(rmse),
+              mean_coverage = mean(cov),
+              sd_coverage = sd(cov)) %>%
+    select(n, nchain, mean_coverage,sd_coverage) %>%
+    mutate(nchain = as.factor(nchain)) %>%
+    ggplot(aes(x = n, y= mean_coverage, color = nchain))+
+    geom_line()+
+    geom_errorbar(aes(ymin = mean_coverage - sd_coverage, ymax = mean_coverage + sd_coverage), width = 3e3) +
+    labs(y = "Empirical Coverage (1000 test pts)", title = paste0("DGP: ",dgp))+
+    geom_hline(yintercept = .95,linetype="dashed",
+               color = "red")+
+    theme_classic() -> plot_i
+  all_plots[[iteration]] = plot_i
+  ggsave(plot_i,filename = paste0("results/figures/coverage/",dgp,"coverage.pdf"), height = 4, width = 6)
+
+  iteration <- iteration+1
 }
 
-results[-1,] %>% 
-  filter(n>100) %>% 
-  group_by(n, nchain, exp) %>% 
-  summarise(mean_rmse = mean(rmse),
-            mean_coverage = mean(cov),
-            mean_wink = mean(wink)) %>% 
-  select(n, nchain, mean_wink) %>% 
-  mutate(nchain = as.factor(nchain)) %>% 
-  ggplot(aes(x = n, y= mean_wink, color = nchain))+
-  geom_line()+
-  labs(y = "Mean Winkler Score (1000 test pts)", title = paste0("DGP: ",dgp))+
-  theme_classic()
-ggsave(filename = paste0("results/figures/coverage/",dgp,"winkler.pdf"), height = 4, width = 6)
+library(gridExtra)
 
-results[-1,] %>% 
-  filter(n>100) %>% 
-  group_by(n, nchain, exp) %>% 
-  summarise(mean_rmse = mean(rmse),
-            mean_coverage = mean(cov),
-            mean_wink = mean(wink)) %>% 
-  select(n, nchain, mean_coverage) %>% 
-  mutate(nchain = as.factor(nchain)) %>% 
-  ggplot(aes(x = n, y= mean_coverage, color = nchain))+
-  geom_line()+
-  labs(y = "Empirical Coverage (1000 test pts)", title = paste0("DGP: ",dgp))+
-  geom_hline(yintercept = .95,linetype="dashed", 
-             color = "red")+
-  theme_classic()
-ggsave(filename = paste0("results/figures/coverage/",dgp,"coverage.pdf"), height = 4, width = 6)
-
+p_final = grid.arrange(all_plots[[1]],all_plots[[2]],
+             all_plots[[3]],all_plots[[4]],
+             all_plots[[5]],all_plots[[6]], nrow = 3)
+ggsave(p_final,filename = paste0("results/figures/coverage/all_coverages.pdf"), height = 11, width = 8)
--- code/postprocess_rmse.R
+++ code/postprocess_rmse.R
@@ -1,38 +1,37 @@
 library(ggplot2)
 library(dplyr)
 
-# Should be one of sum,high, low, and piecewise, tree, and lss
-dgp = "tree"
-
 all_plots = list()
+
+# Unused for now
+calc_winkler <- function(Y,
+                         ci.upper,
+                         ci.lower) {
+  if (length(Y) != length(ci.lower) | length(Y) != length(ci.upper)) {
+    stop("Lengths of inputs don't match")
+  }
+  
+  # First calculate if the CI covers each observation
+  over <- ifelse(Y > ci.upper,
+                 Y - ci.upper,0)
+  
+  under <- ifelse(Y < ci.lower,
+                  ci.lower - Y,0)
+  
+  # Winkler
+  winkler <- ifelse(over > 0,
+                    ci.upper-ci.lower + 40*over, # If over upper limit, pay 2 / .05 * over penalty
+                    ifelse(under > 0,
+                           ci.upper-ci.lower + 40*over, # If below lower limit, pay 2 / .05 * under penalty
+                           ci.upper-ci.lower # Otherwise just pay interval length
+                    )
+  )
+  return(mean(winkler))
+}
 
 iteration = 1
 for (dgp in c("sum","high", "low", "piecewise", "tree", "lss")) {
-  calc_winkler <- function(Y,
-                           ci.upper,
-                           ci.lower) {
-    if (length(Y) != length(ci.lower) | length(Y) != length(ci.upper)) {
-      stop("Lengths of inputs don't match")
-    }
 
-    # First calculate if the CI covers each observation
-    over <- ifelse(Y > ci.upper,
-                   Y - ci.upper,0)
-
-    under <- ifelse(Y < ci.lower,
-                    ci.lower - Y,0)
-
-    # Winkler
-    winkler <- ifelse(over > 0,
-                      ci.upper-ci.lower + 40*over, # If over upper limit, pay 2 / .05 * over penalty
-                      ifelse(under > 0,
-                             ci.upper-ci.lower + 40*over, # If below lower limit, pay 2 / .05 * under penalty
-                             ci.upper-ci.lower # Otherwise just pay interval length
-                      )
-    )
-    return(mean(winkler))
-
-  }
 
   results <- data.frame(n = NA,
                         nchain = NA,
@@ -41,14 +40,10 @@ for (dgp in c("sum","high", "low", "piecewise", "tree", "lss")) {
                         rmse = NA,
                         cov = NA)
 
-
-  # file = "dgp_high_run_8_n_10000_coverage.RDS"
-  # f <- readRDS(file = paste0("results/coverage/",file))
-
-  for (file in dir("results/coverage2/")) {
+  for (file in dir("results/coverage/")) {
     if (grepl(pattern = dgp, x = file)) {
       #print(file)
-      f <- readRDS(file = paste0("results/coverage2/",file))
+      f <- readRDS(file = paste0("results/coverage/",file))
 
       for (iter in 1:3) {
         n_i = f[[1]]$n
@@ -72,21 +67,7 @@ for (dgp in c("sum","high", "low", "piecewise", "tree", "lss")) {
     }
   }
 
-  # results[-1,] %>%
-  #   filter(n>100) %>%
-  #   group_by(n, nchain, exp) %>%
-  #   summarise(mean_rmse = mean(rmse),
-  #             mean_coverage = mean(cov),
-  #             mean_wink = mean(wink)) %>%
-  #   select(n, nchain, mean_wink) %>%
-  #   mutate(nchain = as.factor(nchain)) %>%
-  #   ggplot(aes(x = n, y= mean_wink, color = nchain))+
-  #   geom_line()+
-  #   labs(y = "Mean Winkler Score (1000 test pts)", title = paste0("DGP: ",dgp))+
-  #   theme_classic()
-  # ggsave(filename = paste0("results/figures/coverage/",dgp,"winkler.pdf"), height = 4, width = 6)
-
-  results %>%
+  results[-1,] %>%
     group_by(n, nchain, exp) %>%
     summarise(reps_completed = max(run)) -> num_runs
   print(dgp)
@@ -94,28 +75,26 @@ for (dgp in c("sum","high", "low", "piecewise", "tree", "lss")) {
 
   results[-1,] %>%
     filter(n>100) %>%
-    group_by(n) %>%
-    mutate(rmse_one_chain = mean(rmse[nchain==1])) %>%
+    group_by(n, exp,run) %>%
+    mutate(rmse_one = rmse[nchain==1]) %>% 
+    ungroup() %>% 
+    mutate(rmse = rmse / rmse_one) %>%
     group_by(n, nchain, exp) %>%
     summarise(mean_rmse = mean(rmse),
               mean_coverage = mean(cov),
               sd_coverage = sd(cov),
-              mean_rmse = mean(rmse)/rmse_one_chain,
-              sd_rmse = sd(rmse)/rmse_one_chain) %>%
+              mean_rmse = mean(rmse),
+              sd_rmse = sd(rmse)) %>%
     group_by(n) %>%
-    mutate(mean_rmse = mean_rmse / mean_rmse[nchain==1],
-           sd_rmse = sd_rmse / mean_rmse[nchain==1]) %>%
     select(n, nchain, mean_rmse,sd_rmse) %>%
     mutate(nchain = as.factor(nchain)) %>%
     ggplot(aes(x = n, y= mean_rmse, color = nchain))+
     geom_line()+
     geom_errorbar(aes(ymin = mean_rmse - sd_rmse, ymax = mean_rmse + sd_rmse), width = 3e3) +
     labs(y = "RMSE/RMSE(1 chain) (1000 test pts)", title = paste0("DGP: ",dgp))+
-    #geom_hline(yintercept = .95,linetype="dashed",
-    #           color = "red")+
     theme_classic() -> plot_i
   all_plots[[iteration]] = plot_i
-  ggsave(plot_i,filename = paste0("results/figures/coverage2/",dgp,"rmse.pdf"), height = 4, width = 6)
+  ggsave(plot_i,filename = paste0("results/figures/coverage/",dgp,"rmse.pdf"), height = 4, width = 6)
 
   iteration <- iteration+1
 }
@@ -125,4 +104,4 @@ library(gridExtra)
 grid.arrange(all_plots[[1]],all_plots[[2]],
              all_plots[[3]],all_plots[[4]],
              all_plots[[5]],all_plots[[6]], nrow = 3)->p_final
-ggsave(p_final,filename = paste0("results/figures/coverage2/all_rmse.pdf"), height = 11, width = 8)
+ggsave(p_final,filename = paste0("results/figures/coverage/all_rmse.pdf"), height = 11, width = 8)
